4a81df8d56bff27a32d008bc06cc6981b78c06b4
Oryntayev-Mukhammed <hinatarocklee@gmail.com>
2022-12-23 10:48:44 +0600

    First commit

---
+++ src/Components/Comment.js
@@ -0,0 +1,349 @@
+import { useState } from "react";
+
+import "./Styles/Comment.scss";
+import "./Styles/AddComment.scss";
+import "./Styles/DeleteModal.scss";
+
+import { CommentHeader, CommentFooter, CommentVotes } from "./commentParts";
+
+/* Добавление комментария */
+const AddComment = ({ buttonValue, addComments, replyingTo }) => {
+  const replyingToUser = replyingTo ? `@${replyingTo}, ` : "";
+  const [comment, setComment] = useState(replyingToUser);
+
+  const clickHandler = () => {
+    if (comment === "" || comment === " ") return;
+
+    const newComment = {
+      id: Math.floor(Math.random() * 100) + 5,
+      content: replyingToUser + comment.replace(replyingToUser, ""),
+      createdAt: new Date(),
+      score: 0,
+      username: "juliusomo",
+      currentUser: true,
+      replies: [],
+    };
+
+    addComments(newComment);
+    setComment("");
+  };
+
+  return (
+    <div className="add-comment">
+      <div className="profile-pic"></div>
+      <textarea
+        className="comment-input"
+        placeholder="Add a comment"
+        value={comment}
+        onChange={(e) => {
+          setComment(e.target.value);
+        }}
+      />
+      <div className="send-btn-container">
+        <div className="profile-pic"></div>
+        <button className="add-btn" onClick={clickHandler}>
+          {buttonValue}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+/* Добавление комментария */
+
+/* Блок ответа */
+const ReplyContainer = ({
+  commentData,
+  updateScore,
+  addReply,
+  editComment,
+  deleteComment,
+  setDeleteModalState,
+}) => {
+  return (
+    <div className="reply-container">
+      {commentData.map((data) => (
+        <Reply
+          key={data.id}
+          commentData={data}
+          updateScore={updateScore}
+          addNewReply={addReply}
+          editComment={editComment}
+          deleteComment={deleteComment}
+          setDeleteModalState={setDeleteModalState}
+        />
+      ))}
+    </div>
+  );
+};
+/* Блок ответа */
+
+
+/* Удаление модалки */
+const DeleteModal = ({ setDeleting, deleteComment, setDeleteModalState }) => {
+  const cancelDelete = () => {
+    setDeleting(false);
+    setDeleteModalState(false);
+  };
+
+  const deleteBtnClick = () => {
+    deleteComment();
+    setDeleteModalState(false);
+  };
+
+  return (
+    <div className="delete-confirmation-wrapper">
+      <div className="delete-container">
+        <div className="title">Delete comment</div>
+        <div className="confirmation-message">
+          Are you sure you want to delete this comment? This will remove the
+          comment and can't be undone.
+        </div>
+        <div className="btn-container">
+          <button className="cancel-btn" onClick={cancelDelete}>
+            No, cancel
+          </button>
+          <button className="delete-btn" onClick={deleteBtnClick}>
+            Yes, delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+/* Удаление модалки */
+
+
+/* Ответы */
+const Reply = ({
+  commentData,
+  updateScore,
+  addNewReply,
+  editComment,
+  deleteComment,
+  setDeleteModalState,
+}) => {
+  const [replying, setReplying] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [content, setContent] = useState(commentData.content);
+  const [deleting, setDeleting] = useState(false);
+
+  // adding reply
+  const addReply = (newReply) => {
+    addNewReply(newReply);
+    setReplying(false);
+  };
+
+  const commentContent = () => {
+    const text = commentData.content.trim().split(" ");
+    const firstWord = text.shift().split(",");
+
+    return !editing ? (
+      <div className="comment-content">
+        <span className="replyingTo">{firstWord}</span>
+        {text.join(" ")}
+      </div>
+    ) : (
+      <textarea
+        className="content-edit-box"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      />
+    );
+  };
+
+  const updateComment = () => {
+    editComment(content, commentData.id, "reply");
+    setEditing(false);
+  };
+
+  // delete comment
+  const deleteReply = () => {
+    deleteComment(commentData.id, "reply");
+    setDeleting(false);
+  };
+
+  return (
+    <div
+      className={`comment-container ${
+        commentData.replies[0] !== undefined ? "gap" : ""
+      }`}
+    >
+      <div className="comment">
+        <CommentVotes
+          updateScore={updateScore}
+          commentData={commentData}
+          type="reply"
+        />
+        <div className="comment--body">
+          <CommentHeader
+            commentData={commentData}
+            setReplying={setReplying}
+            setDeleting={setDeleting}
+            setDeleteModalState={setDeleteModalState}
+            setEditing={setEditing}
+          />
+
+          {commentContent()}
+          {editing && (
+            <button className="update-btn" onClick={updateComment}>
+              update
+            </button>
+          )}
+        </div>
+        <CommentFooter
+          updateScore={updateScore}
+          commentData={commentData}
+          setReplying={setReplying}
+          setDeleting={setDeleting}
+          setDeleteModalState={setDeleteModalState}
+          setEditing={setEditing}
+          type="reply"
+        />
+      </div>
+
+      {replying && (
+        <AddComment
+          buttonValue={"reply"}
+          addComments={addReply}
+          replyingTo={commentData.username}
+        />
+      )}
+      {commentData.replies.map((reply) => (
+        <Reply key={reply.id} commentData={reply} addReply={addReply} />
+      ))}
+
+      {deleting && (
+        <DeleteModal
+          setDeleting={setDeleting}
+          deleteComment={deleteReply}
+          setDeleteModalState={setDeleteModalState}
+        />
+      )}
+    </div>
+  );
+};
+/* Ответы */
+
+
+
+const Comment = ({
+  commentData,
+  updateScore,
+  updateReplies,
+  editComment,
+  commentDelete,
+  setDeleteModalState,
+}) => {
+  const [replying, setReplying] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [content, setContent] = useState(commentData.content);
+  const [deleting, setDeleting] = useState(false);
+
+  const addReply = (newReply) => {
+    const replies = [...commentData.replies, newReply];
+    updateReplies(replies, commentData.id);
+    setReplying(false);
+  };
+
+  const updateComment = () => {
+    editComment(content, commentData.id, "comment");
+    setEditing(false);
+  };
+
+  const deleteComment = (id, type) => {
+    const finalType = type !== undefined ? type : "comment";
+    const finalId = id !== undefined ? id : commentData.id;
+    commentDelete(finalId, finalType, commentData.id);
+    setDeleting(false);
+  };
+
+  return (
+    <div
+      className={`comment-container ${
+        commentData.replies[0] !== undefined ? "reply-container-gap" : ""
+      }`}
+    >
+      <div className="comment">
+        <CommentVotes
+          updateScore={updateScore}
+          commentData={commentData}
+          type="comment"
+        />
+        <div className="comment--body">
+          <CommentHeader
+            commentData={commentData}
+            replying={replying}
+            setReplying={setReplying}
+            setDeleting={setDeleting}
+            setDeleteModalState={setDeleteModalState}
+            setEditing={setEditing}
+          />
+          {!editing ? (
+            <div className="comment-content">{commentData.content}</div>
+          ) : (
+            <textarea
+              className="content-edit-box"
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            />
+          )}
+          {editing && (
+            <button className="update-btn" onClick={updateComment}>
+              update
+            </button>
+          )}
+        </div>
+        <CommentFooter
+          updateScore={updateScore}
+          commentData={commentData}
+          setReplying={setReplying}
+          setDeleting={setDeleting}
+          setDeleteModalState={setDeleteModalState}
+          setEditing={setEditing}
+          type="comment"
+        />{" "}
+      </div>
+
+      {replying && (
+        <AddComment
+          buttonValue={"reply"}
+          addComments={addReply}
+          replyingTo={commentData.username}
+        />
+      )}
+      {commentData.replies !== [] && (
+        <ReplyContainer
+          key={commentData.replies.id}
+          commentData={commentData.replies}
+          updateScore={updateScore}
+          addReply={addReply}
+          editComment={editComment}
+          deleteComment={deleteComment}
+          setDeleteModalState={setDeleteModalState}
+        />
+      )}
+
+      {deleting && (
+        <DeleteModal
+          setDeleting={setDeleting}
+          deleteComment={deleteComment}
+          setDeleteModalState={setDeleteModalState}
+        />
+      )}
+    </div>
+  );
+};
+
+export {
+  Comment,
+  Reply,
+  AddComment,
+  DeleteModal,
+  ReplyContainer,
+}
+
